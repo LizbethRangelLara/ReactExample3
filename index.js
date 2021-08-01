69124920ee40aae6f66ed88f7a56aee85f82ceeb
LizbethRangelLara <84157429+LizbethRangelLara@users.noreply.github.com>
2021-08-01 12:05:10 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const CharacterCounter = () => {
+  const [inputValue, setInputValue] = useState('');
+  
+  const handleChange = (event) => {
+    const element = event.target;
+    setInputValue(element.value);
+  };
+  
+  return (
+    <div>
+      <textarea cols={80} rows={10} value={inputValue} onChange={handleChange} />
+      <div>Count: {inputValue.length}</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<CharacterCounter />, mountNode);
